e86634315c0b4364f1154140bcfa7b08c86ce39d
Nura8 <bel_anna@list.ru>
2019-04-14 20:51:34 +0300

    +1

--- task/06-conditions-n-loops-tasks.js
+++ task/06-conditions-n-loops-tasks.js
@@ -404,7 +404,7 @@ function timespanToHumanString(startDate, endDate) {
  *    365, 10 => '365'
  */
 function toNaryString(num, n) {
-    throw new Error('Not implemented');
+    return Number(num).toString(n);
 }
 
 
